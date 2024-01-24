d93efd85cb4d580b31ce53546fefcb5acd308c0d
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2024-01-24 23:56:54 +0530

    feat: used test, production & development environment

--- src/config/database.ts
+++
@@ -1,9 +0,0 @@
-import { DATABASE_URI } from '../constants';
-import { DatabaseService } from '../services';
-import { MongoDBConnector } from '../database/MongoDBConnector';
-
-const DB_OPTIONS = {
-  dbName: 'ansopedia-user',
-};
-
-export const db = DatabaseService.getInstance(new MongoDBConnector(DATABASE_URI, DB_OPTIONS));
